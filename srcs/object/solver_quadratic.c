bd1d64751c5ac26fcd03fb1f633752d6c0611ba7
Ctsu27 <kehuang42student@gmail.com>
2019-01-27 13:45:31 +0100

    fix: solver quadratic

--- srcs/object/solver_quadratic.c
+++ srcs/object/solver_quadratic.c
@@ -6,7 +6,7 @@
 /*   By: kehuang <kehuang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/27 01:56:01 by kehuang           #+#    #+#             */
-/*   Updated: 2019/01/27 13:28:36 by kehuang          ###   ########.fr       */
+/*   Updated: 2019/01/27 13:44:51 by kehuang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ int		solver_quadratic(const double a, const double b, const double c,
 	if (delta < 0.0)
 		return (0);
 	delta = sqrt(delta);
-	dis[0] = (b + delta) / (a * 0.5);
-	dis[1] = (b - delta) / (a * 0.5);
+	dis[0] = (-b + delta) / (a * 0.5);
+	dis[1] = (-b - delta) / (a * 0.5);
 	if (dis[0] < 0.01 && dis[1] < 0.01)
 		return (0);
 	*t = (dis[0] < dis[1]) ? dis[0] : dis[1];
