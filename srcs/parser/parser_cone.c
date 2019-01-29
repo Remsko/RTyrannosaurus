3d73f652b849a30dbc1b74dd4ccc51fe3ddd27a4
Remsko <rpinoit@student.42.fr>
2019-01-29 12:56:35 +0100

    cone radius fix

--- srcs/parser/parser_cone.c
+++ srcs/parser/parser_cone.c
@@ -6,7 +6,7 @@
 /*   By: rpinoit <rpinoit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/27 18:13:41 by rpinoit           #+#    #+#             */
-/*   Updated: 2019/01/27 18:14:55 by rpinoit          ###   ########.fr       */
+/*   Updated: 2019/01/29 12:47:56 by rpinoit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include "json_types.h"
 #include "json_getter.h"
 
+#include "utils.h"
 #include "object.h"
 #include "parser.h"
 
@@ -28,6 +29,7 @@ void    *parser_cone(t_json_object *o)
     cone->rot = parser_vector(getter_by_key(o, "rot"));
     nerf_rotation(&cone->rot);
     cone->radius = parser_double(getter_by_key(o, "radius"));
+    cone->radius = degree_to_radian(cone->radius);
     //sphere->mater = parser_mater(getter_by_key(o, "mater"));
     return ((void *)cone);
 }
